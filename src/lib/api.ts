302462d0b905a77ef082197ef71ebd177464f770
PratikMbade <pratikmbade18@gmail.com>
2025-12-26 23:30:10 +0530

    fixes api url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_NEW_BACKEND_URL,
+  baseURL: `${process.env.NEXT_PUBLIC_NEW_BACKEND_URL}/api`,
   timeout: 30000,
   headers: {
     'Content-Type': 'application/json',
